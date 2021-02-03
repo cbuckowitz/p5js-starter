861961665a2cdb39d98195974f362fb8fffb7fe7
Christian Buckowitz <cbuckowitz@gmail.com>
2021-02-03 00:38:47 +0000

    ml5 puffin, global d ts

--- sketch.js
+++ sketch.js
@@ -1,14 +1,43 @@
+/// <reference path="./lib/global.d.ts" />)
 
+let mobilenet;
 
+let puffin;
+
+function preload() {
+  puffin = loadImage('images/puffin.jpg');
+}
 
 function setup() {
   // put setup code here
+  createCanvas(640, 480);
+  background(0);
 
+  image(puffin, 0, 0, width, height);
+
+  mobilenet = ml5.imageClassifier('MobileNet', () => {
+    console.log('Model is ready!!!!');
+
+    mobilenet.predict(puffin, (error, results) => {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log(results);
+        fill(0);
+        textSize(64);
+        results.forEach(result => {
+          createP(`${result.label} ${result.confidence}`);
+        });
+      }
+    })
+  });
 }
+
 
 function draw() {
   // put drawing code here
-  
+
+
 
 
 }
